923fc0d6f0f4360b1ea772949d3c74bb139b9b2e
Tomasz Grabusiński <tomasz.grabusinski@xebia.com>
2025-12-09 22:54:12 +0100

    Add documentation for modules in Terraform configuration

--- templates/bootstrap/modules/azure_devops/repository.tf
+++ templates/bootstrap/modules/azure_devops/repository.tf
@@ -18,50 +18,50 @@ resource "azuredevops_git_repository_file" "alz" {
   overwrite_on_create = true
 }
 
-resource "azuredevops_branch_policy_merge_types" "alz" {
-  depends_on = [azuredevops_git_repository_file.alz]
-  project_id = local.project_id
-
-  enabled  = var.create_branch_policies
-  blocking = true
-
-  settings {
-    allow_squash                  = true
-    allow_rebase_and_fast_forward = false
-    allow_basic_no_fast_forward   = false
-    allow_rebase_with_merge       = false
-
-    scope {
-      repository_id  = azuredevops_git_repository.alz.id
-      repository_ref = azuredevops_git_repository.alz.default_branch
-      match_type     = "Exact"
-    }
-  }
-}
-
-locals {
-  build_validation_pipelines = { for key, pipeline in local.pipelines : key => pipeline if lookup(pipeline, "build_validation", false) }
-
-  build_validation_pipeline_key = one(keys(local.build_validation_pipelines))
-}
-
-resource "azuredevops_branch_policy_build_validation" "alz" {
-  count      = length(local.build_validation_pipelines) > 0 ? 1 : 0
-  depends_on = [azuredevops_git_repository_file.alz]
-  project_id = local.project_id
-
-  enabled  = var.create_branch_policies
-  blocking = true
-
-  settings {
-    display_name        = "Terraform Validation"
-    build_definition_id = azuredevops_build_definition.pipeline[local.build_validation_pipeline_key].id
-    valid_duration      = 720
-
-    scope {
-      repository_id  = azuredevops_git_repository.alz.id
-      repository_ref = azuredevops_git_repository.alz.default_branch
-      match_type     = "Exact"
-    }
-  }
-}
+# resource "azuredevops_branch_policy_merge_types" "alz" {
+#   depends_on = [azuredevops_git_repository_file.alz]
+#   project_id = local.project_id
+# 
+#   enabled  = var.create_branch_policies
+#   blocking = true
+# 
+#   settings {
+#     allow_squash                  = true
+#     allow_rebase_and_fast_forward = false
+#     allow_basic_no_fast_forward   = false
+#     allow_rebase_with_merge       = false
+# 
+#     scope {
+#       repository_id  = azuredevops_git_repository.alz.id
+#       repository_ref = azuredevops_git_repository.alz.default_branch
+#       match_type     = "Exact"
+#     }
+#   }
+# }
+# 
+# locals {
+#   build_validation_pipelines = { for key, pipeline in local.pipelines : key => pipeline if lookup(pipeline, "build_validation", false) }
+# 
+#   build_validation_pipeline_key = one(keys(local.build_validation_pipelines))
+# }
+# 
+# resource "azuredevops_branch_policy_build_validation" "alz" {
+#   count      = length(local.build_validation_pipelines) > 0 ? 1 : 0
+#   depends_on = [azuredevops_git_repository_file.alz]
+#   project_id = local.project_id
+# 
+#   enabled  = var.create_branch_policies
+#   blocking = true
+# 
+#   settings {
+#     display_name        = "Terraform Validation"
+#     build_definition_id = azuredevops_build_definition.pipeline[local.build_validation_pipeline_key].id
+#     valid_duration      = 720
+# 
+#     scope {
+#       repository_id  = azuredevops_git_repository.alz.id
+#       repository_ref = azuredevops_git_repository.alz.default_branch
+#       match_type     = "Exact"
+#     }
+#   }
+# }
--- templates/examples/app_with_storage/infrastructure/terraform.tf
+++ templates/examples/app_with_storage/infrastructure/terraform.tf
@@ -26,6 +26,7 @@ terraform {
 
 provider "azurerm" {
   subscription_id = var.subscription_id
+  resource_provider_registrations = "none"
 
   features {
     resource_group {
